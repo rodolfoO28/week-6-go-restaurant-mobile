29b21b0ad3fd72d84011a358696f142ff8c378d7
Rodolfo Oliveira <rdf2806@gmail.com>
2020-06-18 21:03:40 -0300

    Implemented GoRestaurant Mobile challenge

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -23,7 +23,7 @@ interface Food {
   name: string;
   description: string;
   price: number;
-  formattedValue: number;
+  formattedPrice: string;
   thumbnail_url: string;
 }
 
@@ -32,7 +32,22 @@ const Orders: React.FC = () => {
 
   useEffect(() => {
     async function loadOrders(): Promise<void> {
-      // Load orders from API
+      const response = await api.get<Food[]>('orders');
+
+      const ordersFood = response.data.map(
+        ({ id, name, description, price, thumbnail_url }) => {
+          return {
+            id,
+            name,
+            description,
+            price,
+            thumbnail_url,
+            formattedPrice: formatValue(price),
+          };
+        },
+      );
+
+      setOrders(ordersFood);
     }
 
     loadOrders();
